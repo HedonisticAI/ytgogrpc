61c21fef268ae2d690739c82499ccbd0fb1016a2
HedonisticAI <nashandrahedon@gmail.com>
2021-12-16 11:37:59 +0300

    connect, baka

--- ytgo_client/main.go
+++ ytgo_client/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"strings"
+	pb "github.com/HedonisticAI/ytgogrps/proto"
 )
 
 func main() {
+		opts := []grpc.DialOption{
+			grpc.WithInsecure(),
+		}
+		conn, err := grpc.Dial("http://localhost:8080/", opts...)
+		if err != nil {
+			grpclog.Fatalf("fail to dial: %v", err)
+		}
+		defer conn.Close()
 		var s string
 		CreateFolder("pic")
 		checkErr(err)
@@ -14,8 +23,21 @@ func main() {
 		var urls = strings.Split(s, ",")
 		for i:=0; i < len(urls); i++ {
 			//fmt.Println(urls[i])
+			client := pb.NewReverseClient(conn)
+    request := &pb.Request{
+        Message: urls[i],
+    }
+    response, err := client.Do(context.Background(), request)
 
+    if err != nil {
+        grpclog.Fatalf("fail to dial: %v", err)
+    }
+
+   fmt.Println(response.Message)
+			
 		}
+
+
 }
 
 func checkErr(err error) {
